78b951e6887b8516c96b5fa4a28f07062a82d3f5
PaulaEmy <paulaemy8262999@gmail.com>
2026-09-16 11:21:37 -0300

    feat(not-US): Criação do menu lateral

---
+++ frontend/frontend/app/(sistema)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/layout/Navbar/page";
+
+export default function Home() {
+    return (
+        <div>
+
+            <main>
+                <h1>Painel</h1>
+            </main>
+        </div>
+    );
+}
