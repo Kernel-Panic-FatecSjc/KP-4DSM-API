07417edcc1036c74ec25e22994824ab1027344dc
PaulaEmy <paulaemy8262999@gmail.com>
2026-09-24 12:25:37 -0300

    feat(US01-01): Integração front e back da tela de estações

--- kernelpanic-backend/src/estacoes/dto/criar-estacao.dto.ts
+++ kernelpanic-backend/src/estacoes/dto/criar-estacao.dto.ts
@@ -1,4 +1,4 @@
-import { ArrayNotEmpty, IsArray, IsLatitude, IsLongitude, IsNotEmpty, IsString, IsUUID, Matches } from 'class-validator';
+import { IsArray, IsLatitude, IsLongitude, IsNotEmpty, IsString, IsUUID, Matches } from 'class-validator';
 
 const IDENTIFICADOR_UUID_OU_MAC = /^(?:[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}|(?:[0-9a-f]{2}[:-]){5}[0-9a-f]{2})$/i;
 
@@ -22,7 +22,6 @@ export class CriarEstacaoDto {
   longitude!: number;
 
   @IsArray()
-  @ArrayNotEmpty()
   @IsUUID('4', { each: true })
   tipoParametroIds!: string[];
 }
